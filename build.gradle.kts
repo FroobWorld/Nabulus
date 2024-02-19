c26f7824fe884175f96656581c2342cc72dc0e7d
froobynooby <froobynooby@froobworld.com>
2024-02-19 17:10:20 +0930

    Add generated API stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -84,6 +84,13 @@ paperweight {
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
             serverOutputDir.set(layout.projectDirectory.dir("nabulus-server"))
         }
+
+        patchTasks.register("generatedApi") {
+            isBareDirectory = true
+            upstreamDirPath = "paper-api-generator/generated"
+            patchDir = layout.projectDirectory.dir("patches/generated-api")
+            outputDir = layout.projectDirectory.dir("paper-api-generator/generated")
+        }
     }
 }
 
